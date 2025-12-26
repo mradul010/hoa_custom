30be9f83932fe5dc603135be90fcb967a112611d
Mradul Mishra <mradul010@users.noreply.github.com>
2025-12-26 06:53:47 +0000

    Fix item autoname: correct department and company lookups

--- hoa_custom/hooks.py
+++ hoa_custom/hooks.py
@@ -13,7 +13,7 @@ doctype_js = {
 
 doc_events = {
     "Item": {
-        "autoname": "hoa_custom.overrides.item.autoname_item"
+        "autoname": "hoa_custom.overrides.final_item.autoname_item"
     }
 }
 
